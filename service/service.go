4188d032419696df531e8b0c0fe09b39be073ce7
Katherine Nieman <thaigoonch@gmail.com>
2022-02-19 13:26:49 -0700

    add crypto abilities

--- service/service.go
+++ service/service.go
@@ -1,6 +1,12 @@
 package grpcgoonch
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
 	"log"
 
 	"golang.org/x/net/context"
@@ -12,6 +18,56 @@ type Server struct {
 
 func (s *Server) CryptoRequest(ctx context.Context, input *Request) (*DecryptedText, error) {
 	log.Printf("Received text from client: %s", input.Text)
-	log.Printf("Received key from client: %s", input.Key)
-	return &DecryptedText{Result: "Hello From the Goonch Server!"}, nil
+	log.Printf("Received key from client: %v", input.Key)
+
+	encrypted := encrypt(input.Key, input.Text)
+	result := decrypt(input.Key, encrypted)
+
+	return &DecryptedText{Result: result}, nil
+}
+
+func encrypt(key []byte, text string) string {
+	plaintext := []byte(text)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+
+func decrypt(key []byte, cryptoText string) string {
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%v", ciphertext)
 }
