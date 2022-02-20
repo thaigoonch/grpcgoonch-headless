9e7cbd010a9f77fb2e0f8d25eaf015a433668da2
Katherine Nieman <thaigoonch@gmail.com>
2022-02-19 21:32:44 -0700

    k8s side

--- generate.go
+++ generate.go
@@ -1,4 +1,3 @@
-//go:generate bash -c "echo $(which protoc)"
 //go:generate bash -c "docker run -v $(pwd):/app -w /app golang:1.17"
 //go:generate bash -c "protoc --go_out=service --proto_path=$(pwd) --go_opt=paths=source_relative --go-grpc_out=service --go-grpc_opt=paths=source_relative service.proto"
 //go:generate bash -c "./generate.sh"
