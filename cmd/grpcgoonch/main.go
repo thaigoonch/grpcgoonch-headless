93dda4361f4587139f61cc658350188be2cf3274
Katherine Nieman <thaigoonch@gmail.com>
2022-04-07 16:58:41 -0600

    increase max connection

--- cmd/grpcgoonch/main.go
+++ cmd/grpcgoonch/main.go
@@ -52,7 +52,7 @@ func main() {
 	grpcServer := grpc.NewServer(
 		grpc.UnaryInterceptor(grpcMetrics.UnaryServerInterceptor()),
 		grpc.KeepaliveParams(keepalive.ServerParameters{
-			MaxConnectionAge: time.Minute * 2,
+			MaxConnectionAge: time.Minute * 8,
 		}),
 	)
 	grpcgoonch.RegisterServiceServer(grpcServer, &s)
