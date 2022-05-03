8fa9e94336a8142e8d00b0d3bbf7b773df44ba92
Katherine Nieman <katherine.nieman@vivint.com>
2022-05-03 10:41:40 -0600

    changes

--- generate.sh
+++ generate.sh
@@ -3,3 +3,13 @@
 set -e
 
 go mod vendor
+
+protoc \
+    -I. \
+    -I/include/proto \
+    --go_out=service \
+    --proto_path=$(pwd) \
+    --go_opt=paths=source_relative \
+    --go-grpc_out=service \
+    --go-grpc_opt=paths=source_relative \
+    service.proto
